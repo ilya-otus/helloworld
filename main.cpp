3c008e5e6bb6f269f6254bc3870ccb7216718736
Ilya <podshivalov.ilya@yandex.ru>
2018-09-30 09:16:15 +0300

    Remove string_view

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 //#include "lib.h"
 #include <iostream>
 #include <string>
+#include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_sinks.h>
 
 int main(int argc, char *argv[])
 {
     auto logger = spdlog::stdout_logger_mt("console");
     if (argc > 1) {
-        int isVersionRequired = std::string(argv[1]).compare(std::string_view("-v")) == 0;
+        bool isVersionRequired = std::string(argv[1]).compare("-v") == 0;
 	if (isVersionRequired) {
 	    logger->info("version {} was started", 12);
 	}
